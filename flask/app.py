5d0ec998afd5578e0e3411445e1e94f02c48aec0
MattWermers <164249501+MattWermers@users.noreply.github.com>
2026-07-08 21:21:19 -0600

    Update to allow remote debug mode

--- flask/app.py
+++ flask/app.py
@@ -19,4 +19,4 @@ def docs_page():
     return render_template('docs.html', title="WfD - Docs")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
