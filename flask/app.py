4dd97799c5734c31949b4737e0e28ca2f4cd590d
Matthew Wermers <matthew.wermers@gmail.com>
2026-07-09 16:21:47 -0600

    Moved files to clean up repo. Hosting static pages with jekyll on github pages for ease/speed, and dynamic content on AWS, ie. unreitred the webpage.

--- flask/app.py
+++ flask/app.py
@@ -3,20 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def overview():
-    return render_template('about.html', title="WfD - Project Overview")
-
-@app.route('/team')
-def team_page():
-    return render_template('team.html', title="WfD - The Team")
-
-@app.route('/WhatsForDinner')
-def wfd_tool():
+def wfd_home():
     return render_template('WhatsForDinner.html', title="Whats for Dinner")
-
-@app.route('/docs')
-def docs_page():
-    return render_template('docs.html', title="WfD - Docs")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
